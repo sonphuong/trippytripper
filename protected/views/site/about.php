2e14df09d7d3e50a5f63448431127cf91b7aa44c
phuong_ds <phuong_ds@septeni-technology.jp>
2014-06-23 17:08:40 +0700

    not

--- protected/views/site/about.php
+++ protected/views/site/about.php
@@ -3,13 +3,10 @@ $this->pageTitle=Yii::app()->name . ' - About';
 $this->breadcrumbs=array(
 	'About',
 );
-?>
-<?php echo Yii::t('translator','About us');?>
+//echo Yii::t('translator','About us');
 
-<?php
 $send = Yii::app()->queue->send('test');
+
 // receive all available messages for current user and immediately delete them from the queue
-$messages = Yii::app()->queue->receive();
-Nfy::log();
-var_dump($messages);
+//$messages = Yii::app()->queue->receive();
 ?>
