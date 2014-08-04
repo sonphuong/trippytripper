043f1e965f2f453cb34a67253a693ca8687810da
sonphuong <s.phuong@yahoo.com>
2014-08-04 16:26:42 +0700

    searchtrip

--- protected/views/trip/_list.php
+++ protected/views/trip/_list.php
@@ -1,73 +1,57 @@
 <div id="trip_list">
-
-<div class="row">
-    <div class="cell user cell1">
-        <a href="avatar"><img class="avatar" src="<?php echo Yum::module('avatar')->getAvatarThumb($data['avatar']); ?>" width="42" height="42"></a>
-        <div class="user-info">
-            <h3 class="username"><?php echo $data['username']; ?></h3>
-        </div>
-        <div class="user-trust">
-            <div class="rating-container">
-                <?php
-                //$this->widget('PcStarRankWidget', array('modelId' => $data['user_id'], 'modelClassName' => get_class($model)));
-                ?>
-            </div>
-            
-            <div class="row"><?php echo Yii::t('translator','Member since');?>: <?php echo date('d-m-Y',$data['createtime']);?></div>
-            
-            <div class="preferences-container">
-                <span class="blabla prefs tip"></span>
-                <span class="no-smoking prefs tip"></span>
+    <div class="row">
+        <div class="cell user cell1">
+            <a href="avatar"><img class="avatar" src="/<?php echo $data['avatar']; ?>" width="100" height="100"></a>
+            <span class="username"><?php echo $data['username']; ?></span>
+            <div>
+                <div class="rating-container">
+                    <?php
+                    //$this->widget('PcStarRankWidget', array('modelId' => $data['user_id'], 'modelClassName' => get_class($model)));
+                    ?>
+                </div>
+                <div><?php echo Yii::t('translator','Since');?>: <?php echo date('d-m-Y',$data['createtime']);?></div>
             </div>
         </div>
-    </div>
-    <div class="cell cell2">
-        <h4/>
-        <?php
-        $leaveDate = new DateTime($data['leave']);
-        $date = $leaveDate->format('Y-m-d');
-        $time = $leaveDate->format('h:m');
-        $today = date('Y-m-d');
-        $today = new DateTime($today);
-        $interval = $leaveDate->diff($today);
-        if ($interval->days === 0) {
-            echo 'Hôm nay - ' . $time;
-        } elseif ($interval->days === 1) {
-            echo 'Ngày mai - ' . $time;
-        } else {
-            echo $date . ' : ' . $time;
-        }
-        ?>
-        <a href="view/?id=<?php echo $data['id']; ?>" rel="nofollow" class="trip-search-oneresult">
-        <h3 class="fromto" itemprop="name">
-            <span class="trip-roads-stop"><?php echo $data['from']; ?></span>
-            <span class="arrow-ie">→</span>
-            <span class="trip-roads-stop"><?php echo $data['to']; ?></span>
-        </h3>
-        </a>
-        <dl class="geo-from">
-            <dt>
-        <span class="icon icon-marker-green-small">
-            <?php echo Yii::t('translator','Pick up point');?>
-        </span>
-            </dt>
-            <dd class="tip">
-                <?php echo $data['gathering_point']; ?>
-            </dd>
-        </dl>
-    </div>
-    <div class="cell offer cell3">
-        <div class="price price-green" itemprop="location">
-            <strong>
-            <span>
-               <u>đ</u> <?php echo $data['fee']; ?>K
-            </span>
-            </strong>
+        <div class="cell cell2 verticalLine">
+            <div class="date">
+            <?php
+            $leaveDate = new DateTime($data['leave']);
+            $date = $leaveDate->format('Y-m-d');
+            $time = $leaveDate->format('h:m');
+            $today = date('Y-m-d');
+            $today = new DateTime($today);
+            $interval = $leaveDate->diff($today);
+            if ($interval->days === 0) {
+                echo 'Hôm nay - ' . $time;
+            } elseif ($interval->days === 1) {
+                echo 'Ngày mai - ' . $time;
+            } else {
+                echo $date . ' : ' . $time;
+            }
+            ?>
+            </div>
+            <a href="view/?id=<?php echo $data['id']; ?>" rel="nofollow" class="fromTo">
+            <div>
+                <span><?php echo $data['from']; ?></span>
+                <span>→</span>
+                <span><?php echo $data['to']; ?></span>
+            </div>    
+            </a>
+            <div>
+                <span class="pickUpPoint">
+                    <?php echo Yii::t('translator','Pick up point');?>:
+                </span>
+                <strong><?php echo $data['gathering_point']; ?></strong>
+            </div>
+        </div>
+        <div class="cell offer cell3">
+            <div class="price" itemprop="location">
+                <u>đ</u> <?php echo $data['fee']; ?>K
+            </div>
             <span class="priceUnit"><?php echo Yii::t('translator','per passenger');?></span>
-        </div>
-        <div class="availability">
-            <strong><?php echo $data['seat_avail']; ?></strong> <span><?php echo Yii::t('translator','seat(s) left');?></span>
+            <div class="availability">
+                <strong><?php echo $data['seat_avail']; ?></strong> <span><?php echo Yii::t('translator','seat(s) left');?></span>
+            </div>
         </div>
     </div>
-</div>
 </div>
